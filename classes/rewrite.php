8b06a7161cb32d74893a7d97504b2912233bda64
Myles McNamara <myles@smyl.es>
2014-11-24 23:46:11 -0500

    restructure rewrites, use dynamic var var

--- classes/rewrite.php
+++ classes/rewrite.php
@@ -10,7 +10,75 @@ class WP_Login_Flow_Rewrite {
 
 		add_action( 'shutdown', array( $this, 'check_for_updates' ) );
 		add_action( 'admin_init', array( $this, 'preserve_rewrite_rules' ) );
+		add_filter( 'lostpassword_url', array( $this, 'lostpassword_url' ), 9999, 2 );
+		add_filter( 'login_url', array( $this, 'login_url' ), 9999, 2 );
+		add_filter( 'register_url', array( $this, 'register_url' ), 9999, 1 );
+		add_filter( 'site_url', array( $this, 'site_url' ), 9999, 4 );
 
+	}
+
+	/**
+	 * Filter the site URL.
+	 *
+	 * @since @@version
+	 *
+	 * @param string      $url     The complete site URL including scheme and path.
+	 * @param string      $path    Path relative to the site URL. Blank string if no path is specified.
+	 * @param string|null $scheme  Scheme to give the site URL context. Accepts 'http', 'https', 'login',
+	 *                             'login_post', 'admin', 'relative' or null.
+	 * @param int|null    $blog_id Blog ID, or null for the current blog.
+	 *
+	 * @return string
+	 */
+	function site_url( $url, $path, $scheme, $blog_id ){
+// wp-login.php?action=rp - reset-password
+// wp-login.php?action=resetpass - reset-password after setting password with prompt to login
+// wp-login.php?loggedout=true
+// wp-login.php?checkemail=confirm
+// wp-login.php?action=lostpassword&error=expiredkey
+// wp-login.php?action=lostpassword&error=invalidkey
+// wp-login.php?action=resetpass
+// wp-login.php?registration=disabled
+// wp-login.php?checkemail=registered - Registration Complete page
+
+		if( $path === "wp-login.php?action=lostpassword" && $this->get_url( 'lost_pw' ) )
+			$url = $this->get_url( 'lost_pw' );
+
+		if( $path === "wp-login.php?action=register" && $this->get_url( 'register' ) )
+			$url = $this->get_url( 'register' );
+
+		if( $path === "wp-login.php" && $this->get_url( 'login' ) )
+			$url = $this->get_url( 'login' );
+
+		return $url;
+
+	}
+
+	function login_url( $url, $redirect ){
+		if( $redirect ) $redirect = "?redirect_to={$redirect}";
+		if( ! $this->get_url( 'login' ) ) return $url . $redirect;
+		return $this->get_url( 'login' ) . $redirect;
+	}
+
+	function lostpassword_url( $url, $redirect ){
+		if ( $redirect ) $redirect = "?redirect_to={$redirect}";
+		if ( ! $this->get_url( 'lost_pw' ) ) return $url . $redirect;
+		return $this->get_url( 'lost_pw' ) . $redirect;
+	}
+
+	function register_url( $url ){
+		if ( ! $this->get_url( 'register' ) ) return $url;
+		return $this->get_url( 'register' );
+	}
+
+	function get_url( $name ){
+
+		$enabled = get_option( "wplf_rewrite_{$name}" );
+		$slug = get_option( "wplf_rewrite_{$name}_slug" );
+
+		if( ! $enabled || ! $slug ) return false;
+
+		return home_url() . "/{$slug}";
 	}
 
 	function check_for_updates(){
@@ -36,30 +104,46 @@ class WP_Login_Flow_Rewrite {
 
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param null $options
+	 *
+	 * @return bool
+	 */
 	function set_rewrite_rules( $options = NULL ) {
 
 		if( self::$prevent_rewrite ) return false;
 
-		$enable_login = get_option( 'wplf_rewrite_login' );
-		$login_rewrite = get_option( 'wplf_rewrite_login_slug' );
-		$enable_lostpw = get_option( 'wplf_rewrite_lost_pw' );
-		$lostpw_rewrite = get_option( 'wplf_rewrite_lost_pw_slug' );
-		$enable_activate = get_option( 'wplf_rewrite_activate' );
-		$activate_rewrite = get_option( 'wplf_rewrite_activate_slug' );
-		$enable_register = get_option( 'wplf_rewrite_register' );
-		$register_rewrite = get_option( 'wplf_rewrite_register_slug' );
+		$settings = WP_Login_Flow_Settings::get_settings( 'rewrites' );
+		$options = array_column_recursive( $settings, 'name' );
 
-		if ( ! empty( $enable_login ) && ! empty( $login_rewrite ) ) {
-			add_rewrite_rule( $login_rewrite . '/?', 'wp-login.php', 'top' );
+		foreach ( $options as $option ){
+			if( (strlen( $option, '_slug' ) !== false)  || ! get_option( $option ) ) continue; // Skip _slug or disabled
+			${$option} = get_option( "${$option}_slug" ); // wplf_rewrite_lost_pw is @var $lost_pw
 		}
-		if ( ! empty( $enable_lostpw ) && ! empty( $lostpw_rewrite ) ) {
-			add_rewrite_rule( $lostpw_rewrite . '/?', 'wp-login.php?action=lostpassword', 'top' );
+
+		/** @var self $login */
+		if ( $login ) {
+			add_rewrite_rule( $login . '/?', 'wp-login.php', 'top' );
 		}
-		if ( ! empty( $enable_activate ) && ! empty( $activate_rewrite ) ) {
-			add_rewrite_rule( $activate_rewrite . '/([^/]*)/([^/]*)/', 'wp-login.php?action=rp&key=$2&login=$1', 'top' );
+
+		/** @var self $lost_pw */
+		if ( $lost_pw ) {
+			add_rewrite_rule( $lost_pw . '/?', 'wp-login.php?action=lostpassword', 'top' );
 		}
-		if ( ! empty( $enable_register ) && ! empty( $register_rewrite ) ) {
-			add_rewrite_rule( $register_rewrite . '/?', 'wp-login.php?action=register', 'top' );
+
+		/** @var self $activate */
+		if ( $activate ) {
+			add_rewrite_rule( $activate . '/([^/]*)/([^/]*)/', 'wp-login.php?action=rp&key=$2&login=$1', 'top' );
+		}
+
+		/** @var self $register */
+		if ( $register ) {
+			add_rewrite_rule( $register . '/?', 'wp-login.php?action=register', 'top' );
 		}
 	}
 }
