07992930e0fdb8a447b82a905d8d020f22590e6a
Myles McNamara <myles@smyl.es>
2014-11-30 14:14:06 -0500

    working activate to set password rewrite/handling

--- classes/rewrite.php
+++ classes/rewrite.php
@@ -14,8 +14,25 @@ class WP_Login_Flow_Rewrite {
 		add_filter( 'register_url', array( $this, 'register_url' ), 9999, 1 );
 		add_filter( 'site_url', array( $this, 'site_url' ), 9999, 4 );
 		add_filter( 'wp_redirect', array( $this, 'site_url_redirect' ), 9999, 2 );
+		add_action( 'login_form_resetpass', array( $this, 'handle_activate_rewrite' ), 9999 );
+		add_action( 'login_form_rp', array( $this, 'handle_activate_setpw' ), 9999 );
+
 	}
 
+	/**
+	 * Handle wp_redirect rewrites
+	 *
+	 * The core wp-login.php does not use permalinks/rewrites and as such it has
+	 * hard-coded wp-login.php urls.  We use a filter on wp_redirect to redirect
+	 * to a rewrite/permalink if enabled.
+	 *
+	 * @since @@version
+	 *
+	 * @param $location
+	 * @param $status
+	 *
+	 * @return string
+	 */
 	function site_url_redirect( $location, $status ){
 
 		$site_url = get_site_url();
@@ -25,27 +42,50 @@ class WP_Login_Flow_Rewrite {
 		return $location;
 	}
 
-	function check_for_cookie( $url, $path ){
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 */
+	function handle_activate_setpw(){
 
 		$rp_cookie = 'wp-resetpass-' . COOKIEHASH;
-		if ( $path && isset( $_COOKIE[ $rp_cookie ] ) && 0 < strpos( $_COOKIE[ $rp_cookie ], ':' ) ) {
-
-			$activation_slug = get_option( 'wplf_rewrite_activate_slug' );
-			if ( strpos( $path, "/" . $activation_slug . "/" ) !== FALSE ) {
-
-				$path_data = untrailingslashit( str_replace( "/" . $activation_slug . "/", "", $path ) );
-				list( $rp_login, $rp_key ) = explode( ':', wp_unslash( $_COOKIE[ $rp_cookie ] ), 2 );
-				list( $username, $activation_code ) = explode( '/', $path_data, 2 );
-
-				if( $rp_login === $username && $rp_key === $activation_code ){
-					return site_url( 'wp-login.php?action=rp' );
-				}
-
-			}
-
+		// Check for activation/lost password cookie and non-permalink URL
+		if ( $_SERVER[ 'REQUEST_URI' ] === '/wp-login.php?action=rp' && isset( $_COOKIE[ $rp_cookie ] ) && 0 < strpos( $_COOKIE[ $rp_cookie ], ':' ) ) {
+			// Exit if activate rewrite is disabled
+			if( ! get_option( 'wplf_rewrite_activate' ) ) return;
+			$rp_path = 'wp-login.php?action=rp&step=password';
+			$redirect = $this->get_url( 'activate', site_url( $rp_path ), 'password' );
+			$value = wp_unslash( $_COOKIE[ $rp_cookie ] );
+			// Set new cookie under our new path, this is required
+			setcookie( $rp_cookie, $value, 0, $rp_path, COOKIE_DOMAIN, is_ssl(), TRUE );
+			wp_redirect( $redirect );
+			exit();
 		}
 
-		return $url;
+	}
+
+
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 */
+	function handle_activate_rewrite(){
+
+		$activation_key = filter_input( INPUT_GET, 'key', FILTER_SANITIZE_STRING );
+		$activation_user = filter_input( INPUT_GET, 'login', FILTER_SANITIZE_EMAIL );
+		list( $rp_path ) = explode( '?', wp_unslash( $_SERVER[ 'REQUEST_URI' ] ) );
+
+		if ( $activation_key && $activation_user && $rp_path != '/wp-login.php' ) {
+			$redirect = site_url( 'wp-login.php?action=rp&key=' . $activation_key . '&login=' . $activation_user );
+			wp_redirect( $redirect );
+			exit;
+		}
 
 	}
 
@@ -80,31 +120,75 @@ class WP_Login_Flow_Rewrite {
 		// Logout
 		if ( $path === "wp-login.php?loggedout=true" ) return $this->get_url( 'loggedout', $url );
 		// Activate
-		//if ( $path === "wp-login.php?action=rp&activatecookie=set" ) return $this->get_url( 'activate', $url, 'set-password' );
+		//if ( $path === "wp-login.php?action=rp&activatecookie=set" ) return $this->get_url( 'activate', $url, 'setpw' );
 
-		$url = $this->check_for_cookie( $url, $path );
+		#$url = $this->check_for_cookie( $url, $path );
 
 		return $url;
 
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param $url
+	 * @param $redirect
+	 *
+	 * @return string
+	 */
 	function login_url( $url, $redirect ){
 		if( $redirect ) $redirect = "?redirect_to={$redirect}";
 		if( ! $this->get_url( 'login' ) ) return $url . $redirect;
 		return $this->get_url( 'login' ) . $redirect;
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param $url
+	 * @param $redirect
+	 *
+	 * @return string
+	 */
 	function lostpassword_url( $url, $redirect ){
 		if ( $redirect ) $redirect = "?redirect_to={$redirect}";
 		if ( ! $this->get_url( 'lost_pw' ) ) return $url . $redirect;
 		return $this->get_url( 'lost_pw' ) . $redirect;
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param $url
+	 *
+	 * @return bool|null|string
+	 */
 	function register_url( $url ){
 		if ( ! $this->get_url( 'register' ) ) return $url;
 		return $this->get_url( 'register' );
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param      $name
+	 * @param null $original_url
+	 * @param null $extra_rewrite
+	 *
+	 * @return bool|null|string
+	 */
 	function get_url( $name, $original_url = null, $extra_rewrite = null ){
 
 		$enabled = get_option( "wplf_rewrite_{$name}" );
@@ -119,6 +203,13 @@ class WP_Login_Flow_Rewrite {
 		return $build_url;
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 */
 	function check_for_updates(){
 
 		$option_page = ( isset( $_POST[ 'option_page' ] ) ? $_POST[ 'option_page' ] : null );
@@ -131,12 +222,30 @@ class WP_Login_Flow_Rewrite {
 
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param $prevent
+	 */
 	static function prevent_rewrite( $prevent ){
 
 		self::$prevent_rewrite = $prevent;
 
 	}
 
+	/**
+	 *
+	 *
+	 *
+	 * @since @@version
+	 *
+	 * @param $option
+	 *
+	 * @return bool
+	 */
 	function filter_rewrite_options( $option ){
 		$rewrite = strpos( $option, 'wplf_rewrite' ) !== FALSE;
 		$slug = strpos( $option, '_slug' ) !== FALSE;
@@ -188,7 +297,7 @@ class WP_Login_Flow_Rewrite {
 
 		/** @var self $activate */
 		if ( $activate ) {
-			// add_rewrite_rule( $activate . '/set-password/?', 'wp-login.php?action=rp', 'top' );
+			add_rewrite_rule( $activate . '/password/?', 'wp-login.php?action=rp&step=password', 'top' );
 			add_rewrite_rule( $activate . '/([^/]*)/([^/]*)/', 'wp-login.php?action=rp&key=$2&login=$1', 'top' );
 		}
 
